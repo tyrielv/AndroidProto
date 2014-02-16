8fc7598c2a683b3317f6aca1a2aaf73f72931ced
Tyrie Vella <tyrielv@gmail.com>
2014-02-15 16:07:11 -0800

    Basic user list

--- TodoList/src/tyrielv/todolist/MainActivity.java
+++ TodoList/src/tyrielv/todolist/MainActivity.java
@@ -1,15 +1,24 @@
 package tyrielv.todolist;
 
+import java.util.ArrayList;
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends ListActivity {	
+	
+	private ArrayList<String> users = new ArrayList<String>();
+	private ArrayAdapter<String> adapter;	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, users); 
+		setListAdapter(adapter);
 	}
 
 	@Override
@@ -17,6 +26,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onCreateNewUserButtonClick(View v) {
+		EditText newUserTextBox = (EditText)findViewById(R.id.newUserTextBox);
+		String newUserName = newUserTextBox.getText().toString();
+		if (newUserName.length() > 0) {
+			users.add(newUserName);
+			adapter.notifyDataSetChanged();
+		}
 	}
 
 }
